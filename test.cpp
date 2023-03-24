8b48ad0ea6db7ef8bce8a500e7f241044ecbee09
tao363 <306236356@qq.com>
2023-03-24 15:37:02 +0800

    master

--- test.cpp
+++ test.cpp
@@ -1,45 +1,56 @@
 #include<iostream>
-#include<algorithm>
+#include<vector>
 #include<string>
+#include <string.h>
+#include <list>
 
 using namespace std;
+/* 基于数组实现的栈 */
+class ArrayStack {
+private:
+    vector<int> stack;
 
-void ComputePrefix(string s,int next[]){
-    int n = s.length();
-    int q,k;
-    next[0] = 0;
-    for(k=0,q=1;q<n;q++){
-        while(k>0 && s[k]!=s[q])
-            k = next[k];
-        if(s[k]==s[q])
-            k++;
-        next[q] = k;
+public:
+    /* 获取栈的长度 */
+    int size() {
+        return stack.size();
     }
-}
-void KMPMatcher(string text,string pattern) {
-    int n = text.length();
-    int m = pattern.length();
-    int next[pattern.length()];
-    ComputePrefix(pattern, next);
-
-    for(int i=0,q=0;i<n;i++) {
-        while(q>0 && pattern[q]!=text[i])
-            q = next[q];
-        if(pattern[q]==text[i])
-            q++;
-        if(q==m)
-        {
-            cout<<"Pattern occurs with shift "<<i-m+1<<endl;
-            q=0;
+    /* 判断栈是否为空 */
+    bool empty() {
+        return stack.empty();
+    }
+    /* 入栈 */
+    void push(int num) {
+        stack.push_back(num);
+    }
+    /* 出栈 */
+    int pop() {
+        if(stack.empty()){
+            throw out_of_range("栈为空,不能执行: pop()函数");
         }
+        int oldTop = stack.back();
+        stack.pop_back();
+        return oldTop;
     }
-}
-
-int main()
-{
-    string s = "abcdabcdebcd";
-    string p  ="bcd";
-    KMPMatcher(s, p);
-    cout<<endl;
-    return 0;
+    /* 访问栈顶元素 */
+    int top() {
+        if(stack.empty()){
+            throw out_of_range("栈为空,不能执行: top()函数");
+        }
+        return stack.back();
+    }
+    /* 访问索引 index 处元素 */
+    int get(int index) {
+        if(stack.size() < index){
+            throw out_of_range("超出栈空间大小");
+        }
+        return stack[index];
+    }
+};
+int main(){
+    ArrayStack sta;
+    cout << sta.empty() << endl;
+   // cout << sta.pop() << endl;
+   // cout << sta.top() << endl;
+   cout << sta.get(2) << endl;
 }
